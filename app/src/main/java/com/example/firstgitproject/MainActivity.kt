e2d7f66878efac682e9ec23077e953cafb0437db
mahdicen <mhdytarmylr@gmail.com>
2022-12-12 21:12:04 +0330

    Adding , removing and editing  server functions added using interface

--- app/src/main/java/com/example/firstgitproject/MainActivity.kt
+++ app/src/main/java/com/example/firstgitproject/MainActivity.kt
@@ -2,13 +2,19 @@ package com.example.firstgitproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.firstgitproject.databinding.ActivityMainBinding
+import com.example.firstgitproject.databinding.DialogAddServerBinding
+import com.example.firstgitproject.databinding.DialogEditServerBinding
 
-class MainActivity : AppCompatActivity() {
-    lateinit var binding :ActivityMainBinding
-    lateinit var serversAdapter :ServersAdapter
+class MainActivity : AppCompatActivity(), ServersEvent {
+    lateinit var binding: ActivityMainBinding
+    lateinit var serversAdapter: ServersAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,23 +29,121 @@ class MainActivity : AppCompatActivity() {
 
         val listOfServers = arrayListOf(
 
-            ServersData("Server One" , "server.comome : 433") ,
-            ServersData("#V2rayFree" , "190.33.80 : 23110") ,
-            ServersData("@CryptoPlus.ir" , "tel.net : 80") ,
-            ServersData("Server Two" , "server.comome : 21") ,
-            ServersData("telyar" , "90.322.1 : 57309") ,
-            ServersData("NeverMore" , "666.666.13 : 99") ,
-            ServersData("Germany!>!" , "styli.xmas") ,
-            ServersData("HPB" , "birthday.op : 34101") ,
-            ServersData("@fakeMailChannel" , "fakeMail.org : 111") ,
+            ServersData("Server One", "server.comome : 433"),
+            ServersData("#V2rayFree", "190.33.80 : 23110"),
+            ServersData("@CryptoPlus.ir", "tel.net : 80"),
+            ServersData("Server Two", "server.comome : 21"),
+            ServersData("telyar", "90.322.1 : 57309"),
+            ServersData("NeverMore", "666.666.13 : 99"),
+            ServersData("Germany!>!", "styli.xmas"),
+            ServersData("HPB", "birthday.op : 34101"),
+            ServersData("@fakeMailChannel", "fakeMail.org : 111"),
 
 
             )
 
-        serversAdapter = ServersAdapter(listOfServers)
+        serversAdapter = ServersAdapter(listOfServers , this)
         binding.recyclerServers.adapter = serversAdapter
-        binding.recyclerServers.layoutManager = LinearLayoutManager(this , RecyclerView.VERTICAL , false)
+        binding.recyclerServers.layoutManager =
+            LinearLayoutManager(this, RecyclerView.VERTICAL, false)
 
+
+        binding.imgAddServer.setOnClickListener {
+
+            val dialog = AlertDialog.Builder(this).create()
+            val dialogBinding = DialogAddServerBinding.inflate(layoutInflater)
+            dialog.setView(dialogBinding.root)
+            dialog.show()
+
+            val listOfPort = listOf(
+
+                "110.20 : 433",
+                "12.10.11 : 2011",
+                "168.190.1.1 : 1128",
+                "serversTel.rip : 80"
+
+            )
+            val adapterMenu = ArrayAdapter(this, R.layout.view_drop_down, listOfPort)
+            (dialogBinding.textInputLayoutMain.editText as AutoCompleteTextView).setAdapter(
+                adapterMenu
+            )
+
+            dialogBinding.btnDoneServer.setOnClickListener {
+
+                if (
+                    dialogBinding.edtNameServer.text!!.isNotEmpty() &&
+                    (dialogBinding.textInputLayoutMain.editText as AutoCompleteTextView).text.isNotEmpty()
+                ) {
+
+                    val name = dialogBinding.edtNameServer.text.toString()
+                    val port =
+                        (dialogBinding.textInputLayoutMain.editText as AutoCompleteTextView).text.toString()
+
+                    val newServer = ServersData(name, port)
+                    serversAdapter.addServer(newServer)
+                    dialog.dismiss()
+
+                } else {
+                    Toast.makeText(this, "fill in the blanks!", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+        }
+
+    }
+
+    override fun onClickDelete(oldServer: ServersData, oldPosition: Int) {
+        serversAdapter.removeServer(oldServer, oldPosition)
+    }
+
+    override fun onClickEdit(server: ServersData, currentPosition: Int) {
+
+        val dialog = AlertDialog.Builder(this).create()
+        val dialogBinding = DialogEditServerBinding.inflate(layoutInflater)
+        dialog.setView(dialogBinding.root)
+        dialog.show()
+
+        val listOfPort = listOf(
+
+            "110.20 : 433",
+            "12.10.11 : 2011",
+            "168.190.1.1 : 1128",
+            "serversTel.rip : 80"
+
+        )
+        val adapterMenu = ArrayAdapter(this, R.layout.view_drop_down, listOfPort)
+        (dialogBinding.textInputLayoutMain.editText as AutoCompleteTextView).setAdapter(
+            adapterMenu
+        )
+
+        dialogBinding.edtNameServer.setText(server.nameServer)
+        (dialogBinding.textInputLayoutMain.editText as AutoCompleteTextView).setText(server.portServer)
+
+        dialogBinding.btnEditServer.setOnClickListener {
+
+            if (
+                (dialogBinding.textInputLayoutMain.editText as AutoCompleteTextView).text.isNotEmpty() &&
+                dialogBinding.edtNameServer.text!!.isNotEmpty()
+            ) {
+
+                val server = ServersData(
+                    dialogBinding.edtNameServer.text.toString(),
+                    (dialogBinding.textInputLayoutMain.editText as AutoCompleteTextView).text.toString()
+                )
+
+                serversAdapter.editServer(server , currentPosition)
+
+            } else {
+                Toast.makeText(this, "fill in the blanks!", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+    }
+
+    override fun onClickShowDetails(server: ServersData, currentPosition: Int) {
+        TODO("Not yet implemented")
     }
 
 }
--- app/src/main/java/com/example/firstgitproject/ServersAdapter.kt
+++ app/src/main/java/com/example/firstgitproject/ServersAdapter.kt
@@ -5,23 +5,38 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.firstgitproject.databinding.ServersItemBinding
+import java.text.FieldPosition
 
-class ServersAdapter(private val data :ArrayList<ServersData>) :RecyclerView.Adapter<ServersAdapter.ServersViewHolder>() {
+class ServersAdapter(private val data: ArrayList<ServersData> , private val serverEvent :ServersEvent) :
+    RecyclerView.Adapter<ServersAdapter.ServersViewHolder>() {
 
-    inner class ServersViewHolder(private val binding :ServersItemBinding , context: Context) :RecyclerView.ViewHolder(binding.root) {
+    inner class ServersViewHolder(private val binding: ServersItemBinding, context: Context) :
+        RecyclerView.ViewHolder(binding.root) {
 
-        fun bindData(position :Int) {
+        fun bindData(position: Int) {
 
             binding.txtNameOfServer.text = data[position].nameServer
             binding.txtPortNumber.text = data[position].portServer
+
+            binding.imgDeleteServer.setOnClickListener {
+                serverEvent.onClickDelete(data[adapterPosition] , adapterPosition)
+            }
+
+            binding.imgEditServer.setOnClickListener {
+                serverEvent.onClickEdit(data[adapterPosition] , adapterPosition)
+            }
+
+            binding.imgShareServer.setOnClickListener {
+                serverEvent.onClickShowDetails(data[adapterPosition] , adapterPosition)
+            }
 
         }
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ServersViewHolder {
-        val binding = ServersItemBinding.inflate(LayoutInflater.from(parent.context) , parent , false)
-        return ServersViewHolder(binding , parent.context)
+        val binding = ServersItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ServersViewHolder(binding, parent.context)
     }
 
     override fun onBindViewHolder(holder: ServersViewHolder, position: Int) {
@@ -35,5 +50,34 @@ class ServersAdapter(private val data :ArrayList<ServersData>) :RecyclerView.Ada
         return data.size
 
     }
+
+    fun addServer(NewServer: ServersData) {
+
+        data.add(0, NewServer)
+        notifyItemInserted(0)
+
+    }
+
+    fun removeServer(oldServer :ServersData , oldPosition:Int) {
+
+        data.remove(oldServer)
+        notifyItemRemoved(oldPosition)
+
+    }
+
+    fun editServer(server: ServersData , position: Int) {
+
+        data.set(position , server)
+        notifyItemChanged(position)
+
+    }
+
+}
+
+interface ServersEvent {
+
+    fun onClickDelete(oldServer :ServersData , oldPosition: Int)
+    fun onClickEdit(server :ServersData , currentPosition: Int)
+    fun onClickShowDetails(server: ServersData , currentPosition: Int)
 
 }
